2e8ea5624f5ec5d63ee88f48f8c12cb6cf807259
Teemu Kallio <teemu.kallio@reaktor.com>
2023-02-03 11:27:16 +0200

    Write to upstream

--- src/client.zig
+++ src/client.zig
@@ -12,15 +12,26 @@ pub const Client = struct {
         std.log.info("New connection: {}", .{self.inConnection.address});
 
         while (true) {
-            var buffer: [1024]u8 = undefined;
-            const len = try self.inConnection.stream.read(buffer[0..]);
+            var inBuffer: [1024]u8 = undefined;
+            const inLen = try self.inConnection.stream.read(inBuffer[0..]);
 
-            if (len == 0) break;
+            if (inLen == 0) break;
 
-            const message = buffer[0..len];
-            // std.debug.print("received: {d} {s}", .{ len, message });
-            var writer = self.outConnection.writer();
-            try writer.writeAll(message);
+            const inMsg = inBuffer[0..inLen];
+            // std.debug.print("in: {d} {s}", .{ inLen, inMsg });
+            var outWriter = self.outConnection.writer();
+            try outWriter.writeAll(inMsg);
+
+            while (true) {
+                var outBuffer: [1024]u8 = undefined;
+                const outLen = try self.outConnection.read(outBuffer[0..]);
+
+                if (outLen == 0) break;
+                const outMsg = outBuffer[0..outLen];
+                // std.debug.print("out: {d} {s}", .{ outLen, outMsg });
+                var inWriter = self.inConnection.stream.writer();
+                try inWriter.writeAll(outMsg);
+            }
         }
 
         self.inConnection.stream.close();
