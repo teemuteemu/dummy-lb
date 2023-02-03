ad2b9f35079e0465184b1b5efc440013f40dd360
Teemu Kallio <teemu.kallio@reaktor.com>
2023-02-03 11:29:37 +0200

    Clean ups

--- src/client.zig
+++ src/client.zig
@@ -11,25 +11,26 @@ pub const Client = struct {
     pub fn handle(self: *Client) !void {
         std.log.info("New connection: {}", .{self.inConnection.address});
 
+        var inBuffer: [1024]u8 = undefined;
+        var outBuffer: [1024]u8 = undefined;
+        var outWriter = self.outConnection.writer();
+        var inWriter = self.inConnection.stream.writer();
+
         while (true) {
-            var inBuffer: [1024]u8 = undefined;
             const inLen = try self.inConnection.stream.read(inBuffer[0..]);
 
             if (inLen == 0) break;
 
             const inMsg = inBuffer[0..inLen];
             // std.debug.print("in: {d} {s}", .{ inLen, inMsg });
-            var outWriter = self.outConnection.writer();
             try outWriter.writeAll(inMsg);
 
             while (true) {
-                var outBuffer: [1024]u8 = undefined;
                 const outLen = try self.outConnection.read(outBuffer[0..]);
 
                 if (outLen == 0) break;
                 const outMsg = outBuffer[0..outLen];
                 // std.debug.print("out: {d} {s}", .{ outLen, outMsg });
-                var inWriter = self.inConnection.stream.writer();
                 try inWriter.writeAll(outMsg);
             }
         }
